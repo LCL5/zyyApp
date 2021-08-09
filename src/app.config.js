75689b8f4f7c8ec076b3605c8b45c3041c6b3dd6
Popular repositories <1223915799@qq.com>
2021-08-09 23:01:34 +0800

    我的挂号，就诊人管理，健康档案，电子病历，就诊费用，点击头像后的那个界面

--- src/app.config.js
+++ src/app.config.js
@@ -1,5 +1,12 @@
 export default {
   pages: [
+    'pages/Cost/cost',
+    'pages/ThePatient/addpatient',
+    'pages/MyRegister/myregister',
+    'pages/Information/information',
+    'pages/MedicalRecord/medicalrecord',
+    'pages/ThePatient/thepatient',
+    'pages/HealthData/healthdata',
     'pages/index/index'
   ],
   window: {
